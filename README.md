611a91fe1f56419a1e60188131c64c76bbd1d56e
Joshua Jahans <joshua.jahans@thehutgroup.com>
2016-10-31 19:58:55 +0000

    Add dbinit and queries files, gitignore app-bundle and app-bundle sourcemap.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Toilet Time
 
 How much have you been paid to poo?
+
+## Usage
+
+`npm install`
+`mongod` to run the database
+`npm run-script server` to run the content server
+`npm run-script <browser|ios|android>` to run the Cordova app on the desired platform
--- server/server.js
+++ server/server.js
@@ -3,17 +3,26 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const MongoClient = require('mongodb').MongoClient;
+const mongoURL = require('./dbconfig');
+const queries = require('./queries');
 const passport = require('passport');
 const headers = require('./headers');
 
+// Initialise database
+require('./dbinit')(MongoClient, mongoURL);
+
+// Add a record
+queries.insertMany();
+
+// Apply headers
 app.use(headers);
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.post('/login', passport.authenticate('local'));
-
 http.listen(3030);
 
+// Add socket events
 require('./events')(io);
