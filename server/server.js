a38a58bf1a7423821eafedde899b5b9cce307dc9
Joshua Jahans <joshua.jahans@thehutgroup.com>
2016-11-07 10:52:02 +0000

    User model methods and update readme formatting

--- server/server.js
+++ server/server.js
@@ -3,16 +3,24 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const session = require('express-session');
+const sessionSecret = require('./auth/secrets').session;
+const cookieParser = require('cookie-parser');
 const mongoURL = require('../database/dbconfig');
 const mongoose = require('../database/mongoose-init')(mongoURL);
 const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
-const facebookStrategy = require('passport-facebook').Strategy;
-const twitterStrategy = require('passport-twitter').Strategy;
 const headers = require('./headers');
 
 // Apply headers
 app.use(headers);
+
+// Enable cookie reading
+app.use(cookieParser());
+
+// Enable sessions
+app.use(session({ secret: sessionSecret }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('Hello');
